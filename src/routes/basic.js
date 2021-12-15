a3b31301dc6aa21d3a88bfffba209a2008aa305d
曲海思 <qu03265610@163.com>
2021-12-15 23:33:12 +0800

    登录增加用户校验能力

--- src/routes/basic.js
+++ src/routes/basic.js
@@ -10,10 +10,18 @@ const router = new Router({ prefix: Api_Prefix });
 router
   .post('/login', async (ctx, next) => {
     const body = ctx.request.body
-    const token = jwt.sign(body, 'asdf')
-    ctx.body = {
-      code: 200,
-      token
+    const user = await models.User.findOne({ where: {loginName: body.loginName}})
+    if(user && user.password === body.password){
+      const token = jwt.sign(body, 'asdf')
+      ctx.body = {
+        code: 200,
+        token
+      }
+    } else {
+      ctx.body = {
+        code: 401,
+        message: '用户名或密码错误'
+      }
     }
   });
 
